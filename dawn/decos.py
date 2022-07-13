43d38cc26db588d3dcb90418a8ec69dac2079e40
Sarthak <shiva02939@gmail.com>
2022-07-13 14:30:34 +0000

    added options kwarg to @slash_command decorator.

--- dawn/decos.py
+++ dawn/decos.py
@@ -1,13 +1,17 @@
 import typing as t
 
-from dawn.slash import SlashCommand
+from dawn.slash import ArgumentOption, SlashCommand
 
+if t.TYPE_CHECKING:
+    from dawn.slash import ArgumentOption
 __all__: t.Tuple[str, ...] = ("slash_command",)
 
 
 def slash_command(
     name: str | None = None,
     description: str | None = None,
+    *,
+    options: t.Sequence[ArgumentOption] | None = None,
     guild_ids: t.Sequence[int] | None = None,
 ) -> t.Callable[[t.Callable], SlashCommand]:
     def inner(callback: t.Callable):
@@ -17,7 +21,9 @@ def slash_command(
             name or callback.__name__,
             description or "No description provided",
             guild_ids,
+            options=tuple(options or []),
         )
+
         slash_command.callback = callback  # type: ignore
 
         return slash_command
