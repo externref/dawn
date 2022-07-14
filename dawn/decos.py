79ca230b42bccfcf8e9c6eb858ea232c4fbee016
Sarthak <shiva02939@gmail.com>
2022-07-14 22:41:30 +0000

    added ansi builder

--- dawn/decos.py
+++ dawn/decos.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 
 import typing as t
@@ -7,7 +6,8 @@ from dawn.slash import Option, SlashCommand
 
 if t.TYPE_CHECKING:
     from dawn.slash import Option
-__all__: t.Tuple[str, ...] = ("slash_command",)
+
+_all__: t.Tuple[str, ...] = ("slash_command",)
 
 
 def slash_command(
@@ -17,6 +17,24 @@ def slash_command(
     options: t.Sequence[Option] | None = None,
     guild_ids: t.Sequence[int] | None = None,
 ) -> t.Callable[[t.Callable], SlashCommand]:
+    """This decorator is used to define a slash command.
+
+    Parameters
+    ----------
+
+        name: :class:`str`
+            Name of the slash command, defaults to the function name
+        description: :class:`str`
+            Slash command description, "No Description provided" is used in case
+            this argument wasn't provided.
+        `options`: :class:`Sequence[.Option]`
+            List of :class:`.Option` objects for the slash command.
+            These represent discord slash command options.
+        `guild_ids`: :class:`Sequence[int]`
+            List of guild IDs to add the command to.
+
+    """
+
     def inner(callback: t.Callable):
         nonlocal name, description, guild_ids
 
