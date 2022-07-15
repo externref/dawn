33babe910d56bd18aea4102a6350a30a4d34c158
Sarthak <shiva02939@gmail.com>
2022-07-15 14:00:40 +0000

    extension setup

--- dawn/__init__.py
+++ dawn/__init__.py
@@ -1,7 +1,8 @@
+from . import helpers
 from .bot import *
 from .context import *
 from .decos import *
+from .extensions import *
 from .slash import *
-from . import helpers
 
 __version__ = "0.0.1"
